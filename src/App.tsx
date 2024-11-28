a9be256b71aacd6d9bc995e10b0a424448f97839
bluemoon983 <msdrobot@naver.com>
2024-11-28 20:43:10 +0900

    path

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Layout from "./components/layout"
 import Home from "./routes/home"
 import Profile from "./routes/profile"
+import Login from "./routes/login"
+import CreateAccount from "./routes/create-account"
 
 const router  = createBrowserRouter([
   {
@@ -17,6 +19,14 @@ const router  = createBrowserRouter([
         element:<Profile/>,
       }
     ]
+  },
+  {
+    path:"/login",
+    element:<Login />,
+  },
+  {
+    path:"/create-account", 
+    element:<CreateAccount />,
   }
 ])
 
