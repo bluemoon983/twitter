ea099f9bf18add79841496f5ba85bc12049534d8
bluemoon983 <msdrobot@naver.com>
2024-11-28 20:55:27 +0900

    GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Home from "./routes/home"
 import Profile from "./routes/profile"
 import Login from "./routes/login"
 import CreateAccount from "./routes/create-account"
+import { createGlobalStyle } from "styled-components"
+import reset from "styled-reset"
 
 const router  = createBrowserRouter([
   {
@@ -30,11 +32,25 @@ const router  = createBrowserRouter([
   }
 ])
 
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing: border-box;
+  }
+  body{
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+`;
 
 function App() {
-  return <>
+  return (
+  <>
+    <GlobalStyle />
     <RouterProvider router={router}/>
   </>
+  );
 }
 
 export default App
